455cf2221966e870defc3aa149152dff4926f8f9
Timothy Waweru <timothywaweru99@gmail.com>
2023-08-23 10:21:14 +0300

    refactors

--- encryption/encryption.ts
+++
@@ -1,9 +0,0 @@
-import * as crypto from 'crypto';
-
-export function encryptPayload(payload: any, ivKey: string, consumerSecret: string): string {
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv('aes-256-cbc', ivKey, iv);
-    let encryptedPayload = cipher.update(JSON.stringify(payload), 'utf8', 'hex');
-    encryptedPayload += cipher.final('hex');
-    return encryptedPayload;
-}
